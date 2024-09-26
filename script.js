6fae2d863f1315f707563551e7a3750d4578e95f
David Martinez <davmartinezb99@gmail.com>
2024-09-26 09:56:45 -0300

    Adicionado, todas as tarefas feitas

--- script.js
+++ script.js
@@ -1,26 +1,77 @@
-let planets = document.getElementById('planets-button');
-let planetData = document.getElementById('planet-data')
+let planets = []
+let planetData = document.getElementById('planetData');
+let searchInput = document.querySelector('input')
+let listPerson = document.getElementById('listPerson')
 
-async function printData(){
+async function printResident(apiLink){
+    
+    let res = await fetch(apiLink);
+    let persona = await res.json();
+    
 
-    let res = await fetch('https://swapi.dev/api/planets/?format=json');
+    let li = document.createElement('li');
 
-    let data = await res.json()
+    li.innerHTML = `<p><strong>Name: </strong>${persona.name}<strong> Birth year: </strong>${persona.birth_year}</p>`;
 
-    console.log(data);    
+    listPerson.appendChild(li);
+}
 
-    data.results.forEach(planet => {
+
+function printData(planet){
+    listPerson.innerHTML = ''
+    planetData.innerHTML = `<h3>${planet.name}</h3>
+    <p><strong>Climate:</strong> ${planet.climate}</p>
+    <p><strong>Population:</strong> ${planet.population}</p>
+    <p><strong>Terreno:</strong> ${planet.terrain}</p>`
+    ;       
+
+    planet.residents.forEach(apiLink =>{
+        apiLinktext = `${apiLink}`;
+        console.log(apiLinktext);
+        printResident(apiLinktext);
+    })
+}
+
+function printButtons(){
+    let planetNames = document.getElementById('planetsButtons');
+    planets.forEach(planet => {
         let button = document.createElement('button');
         button.innerHTML = `${planet.name}`;
-        planets.appendChild(button);
+        planetNames.appendChild(button);
 
         button.addEventListener('click', () =>{
-            planetData.innerHTML = `<h3>${planet.name}</h3>
-            <p><strong>Climate:</strong> ${planet.climate}</p>
-            <p><strong>Population:</strong> ${planet.population}</p>
-            <p><strong>Terrain:</strong> ${planet.terrain}</p>`
-            ;
+            printData(planet);
         });
-    });
+    });           
+       
+}
+
+async function printConsole(){
+    let res = await fetch('https://swapi.dev/api/planets/?format=json');
+    let data = await res.json();
+    planets = data.results;
+    console.log(data.results);    
+    printButtons();
 };
-printData();
+
+
+searchInput.addEventListener("input", e => {
+    let value = e.target.value;
+    finalPlanet = null
+    planets.forEach(planet =>{
+        if (planet.name === value) {
+            finalPlanet = planet
+            printData(planet)    
+        }
+    });
+    if(finalPlanet === null){
+        planetData.innerHTML = `<p>not found</p>`
+        listPerson.innerHTML = ''
+    } else{        
+        printData(finalPlanet)
+        
+    }
+});
+
+printConsole();
+
